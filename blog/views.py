21749a0cd8bfae974b27a3fd43b725ef86e8293b
marina <marinasalome_23@hotmail.com>
2017-10-14 17:25:10 -0300

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
+#En la función render ya tenemos el parámetro request
+#y el archivo 'blog/post_list.html' como plantilla
+#{} es un campo en el que podemos agregar algunas 
+#cosas para que la plantilla las use.	
