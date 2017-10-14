455bc685a80ae9f7e60fc4fbd252de776881f420
marina <marinasalome_23@hotmail.com>
2017-10-14 16:25:25 -0300

    Cambie el HTML para la página.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns =[
+	#Esto significa que para cada URL que empieza con admin/ 
+	#Django encontrará su correspondiente view
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'', include('blog.urls')),
 ]
